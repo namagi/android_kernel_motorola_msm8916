53fc9bcf6429c047ecb547c1d44ab158e5a8664e
ramgear <ramgear@gmail.com>
2013-09-03 14:25:04 +0700

    int_sqrt.c: Correction square root algo with naming
    Change-Id: I762ecaeb863727dd928f4427016bde8d9a7a81c6
    Signed-off-by: AngeloGioacchino Del Regno <kholk11@gmail.com>

--- lib/int_sqrt.c
+++ lib/int_sqrt.c
@@ -1,10 +1,3 @@
-/*
- * Copyright (C) 2013 Davidlohr Bueso <davidlohr.bueso@hp.com>
- *
- *  Based on the shift-and-subtract algorithm for computing integer
- *  square root from Guy L. Steele.
- */
-
 #include <linux/kernel.h>
 #include <linux/export.h>
 
@@ -16,23 +9,33 @@
  */
 unsigned long int_sqrt(unsigned long x)
 {
-	unsigned long b, m, y = 0;
+	unsigned long tmp;
+	unsigned long place;
+	unsigned long root;
+	unsigned long remainder;
 
 	if (x <= 1)
 		return x;
 
-	m = 1UL << (BITS_PER_LONG - 2);
-	while (m != 0) {
-		b = y + m;
-		y >>= 1;
+	root = 0;
+	remainder = x;
+	place = 1UL << (BITS_PER_LONG - 2);
 
-		if (x >= b) {
-			x -= b;
-			y += m;
+	while (place > remainder)
+		place >>= 2;
+
+	while (place != 0) {
+		tmp = root + place;
+
+		if (remainder >= tmp)
+		{
+			remainder -= tmp;
+			root += (place << 1);
 		}
-		m >>= 2;
+		root >>= 1;
+		place >>= 2;
 	}
 
-	return y;
+	return root;
 }
 EXPORT_SYMBOL(int_sqrt);
